312bc108bca73f21b7bc591a21c586f9eb314a41
ERBSON LAVRAS DOS SANTOS <erbson.lavras@globaltec.com.br>
2021-04-24 11:03:08 -0300

    Palindromes can be read in both directions

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Palíndromo
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Lista =
+          {
+            "salas",
+            "socos",
+            "ele",
+            "mirim",
+            "reviver",
+            "level",
+            "Livro",
+            "Nada" };
+
+            foreach (var palavra in Lista)
+            {
+                Console.WriteLine("A palavra :" + palavra + " é um palindrome ?  " + VerificaSeEPalindrome(palavra));
+
+            }
+
+            Console.ReadKey();
+        }
+
+        private static bool VerificaSeEPalindrome(string palavra)
+        {
+            int minimo = 0;
+            int maximo = palavra.Length - 1;
+            while (true)
+            {
+
+                if (minimo > maximo)
+                {
+
+                    return true;
+                }
+
+                Char auxiliar = palavra[minimo];
+                Char auxiliar2 = palavra[maximo];
+
+                if (char.ToLower(auxiliar) != char.ToLower(auxiliar2))
+                {
+
+                    return false;
+                }
+                minimo++;
+                maximo--;
+            }
+
+        }
+
+
+
+
+
+    }
+}
